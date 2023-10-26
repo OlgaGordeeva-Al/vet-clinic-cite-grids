dbe1f3f0398b1ca323394d6cd3729bf5b822b458
Olga Gordeeva <olgagord93@gmail.com>
2023-10-26 17:13:22 +0300

    Added all content components

--- vet-clinic/src/components/Main/Main.tsx
+++ vet-clinic/src/components/Main/Main.tsx
@@ -2,13 +2,15 @@ import "./Main.css"
 import Laboratory from "./Services/Laboratory/Laboratory";
 import Therapy from "./Services/Therapy/Therapy";
 import Ophtalmology from "./Services/Ophtalmology/Ophtalmology";
+import Surgery from "./Services/Surgery/Surgery";
 
 function Main() {
     return (
       <div className="main">
         <Therapy></Therapy>
-        <Laboratory></Laboratory>
+        <Surgery></Surgery>
         <Ophtalmology></Ophtalmology>
+        <Laboratory></Laboratory>
       </div>
     );
   }
---
+++ vet-clinic/src/components/Main/Services/Surgery/Surgery.tsx
@@ -0,0 +1,19 @@
+import "./Surgery.css"
+
+function Surgery() {
+    return (
+      <div className="surgery">
+        <div className="therapy-text">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quae rerum sint, laboriosam officia iusto illo exercitationem dolore dolorum quidem, quas commodi minima pariatur ad corporis facere saepe perspiciatis laudantium:
+          </p>
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit quae rerum sint, laboriosam officia iusto illo exercitationem dolore dolorum quidem, quas commodi minima pariatur ad corporis facere saepe perspiciatis laudantium:
+          </p>
+        </div>
+        <img src="images\pexels-gustavo-fring-6816857.jpg" alt="ophtalmology_photo" className="therapy-image" />
+      </div>
+    );
+  }
+
+  export default Surgery;
