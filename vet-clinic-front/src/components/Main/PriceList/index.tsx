d828a4d08274e25d87658a0136f88c00d2fd257e
Olga Gordeeva <olgagord93@gmail.com>
2024-06-25 11:03:45 +0300

    Commented extra elemtnts in pricelist

--- vet-clinic-front/src/components/Main/PriceList/index.tsx
+++ vet-clinic-front/src/components/Main/PriceList/index.tsx
@@ -2,7 +2,7 @@ import CardList from "../../UI/CardList";
 import HorizontalCard from "../../UI/HorizontalCard"
 import BackbroundCardList from "../../UI/BackgroundCardList"
 import "./index.css";
-import { useState, useEffect } from "react";
+import { useState, useEffect, ReactElement } from "react";
 
 interface Values {
   name: string,
@@ -17,7 +17,8 @@ interface Prices {
 
 const PriceList = () => {
   const [prices, setData] = useState<Prices[]>([]);
-  const [error, setError] = useState<string | null>(null);
+  const [error, setError] = useState<string | null>();
+  const [ActiveComponent, setActiveComponent] = useState<string>("CardList")
 
 
   useEffect(() => {
@@ -33,6 +34,25 @@ const PriceList = () => {
   fetchData();
   }, []);
 
+
+let SomeComponent = CardList;
+
+const handleOnClick = (name: string) => {
+  setActiveComponent(name);
+  console.log(ActiveComponent);
+  if (ActiveComponent=== "CardList") {
+    SomeComponent = CardList;
+  } else if (ActiveComponent === "HorizontalCard") {
+    SomeComponent = HorizontalCard;
+  } else {
+    SomeComponent = BackbroundCardList;
+    console.log(SomeComponent);
+  }
+};
+
+
+const PricePageContent = prices.map((item) => <SomeComponent cardContent={item} key={item.cardName + item.values.length} />);
+
 const PricePage = prices.map((item) => <CardList cardContent={item} key={item.cardName + item.values.length} />);
 
 const PricePageHorisontal = prices.map((item) => <HorizontalCard cardContent={item} key={item.cardName + item.values.length} />);
@@ -42,15 +62,22 @@ const BackgroundPriceList = prices.map((item) => <BackbroundCardList cardContent
   return (
     <div className="price-page">
       <h4 className="page-header price-page__header">Our prices</h4>
+      {/* {PricePageContent} */}
+
       <div className="price-page__content">
         {PricePage}
       </div>
-      <div className="horizontal-pricelist">
+      {/* <div className="horizontal-pricelist">
         {PricePageHorisontal}
       </div>
       <div className="background-pricelist">
         {BackgroundPriceList}
-      </div>
+      </div> */}
+      {/* <div className="priselist-tab">
+        <p onClick={() => handleOnClick("CardList")}>Vertical cards</p>
+        <p onClick={() => handleOnClick("HorizontalCard")}>Horizontal cards</p>
+        <p onClick={() => handleOnClick("BackbroundCardList")}>List with bg</p>
+      </div> */}
     </div>
   )
 }
